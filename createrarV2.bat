88c19e17a354ecbc68569051883f807501f38f47
laijara <fireman0123212@gmail.com>
2025-09-24 21:52:10 +0300

    v 2.0.1 Небольшие исправления

--- createrarV2.bat
+++ createrarV2.bat
@@ -1,5 +1,6 @@
 chcp 65001
-tar.exe acvf coreV2.rar -C "coreV2\CheckHelp\CheckHelp\bin\Debug\net8.0" CheckHelp.exe
+tar.exe acvf coreV2.rar -C "coreV2\CheckHelp\CheckHelp\bin\Release\net8.0\publish\win-x64" CheckHelp.exe
 xcopy "D:\gitupload\CheckHelp\coreV2.rar" "D:\yandexdisk\disksync\YandexDisk\work\Другое\OtherUsers\1"
+xcopy "D:\gitupload\CheckHelp\coreV2\CheckHelp\CheckHelp\bin\Release\net8.0\publish\win-x64\CheckHelp.exe" "D:\gitupload\CheckHelp"
 chcp 866
 pause
