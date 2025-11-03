432a3c2492c319d8472c48fa27b09a5b58829f94
laijara <fireman0123212@gmail.com>
2025-11-03 22:44:20 +0300

    v2.0.5

--- createrar.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-tar.exe acvf core.rar core
-xcopy "D:\gitupload\CheckHelp\core.rar" "D:\yandexdisk\disksync\YandexDisk\work\Другое\OtherUsers\1"
-chcp 866
-pause
--- createrarV2.bat
+++
@@ -1,6 +0,0 @@
-chcp 65001
-tar.exe acvf coreV2.rar -C "coreV2\CheckHelp\CheckHelp\bin\Release\net8.0\publish\win-x64" CheckHelp.exe
-xcopy "D:\gitupload\CheckHelp\coreV2.rar" "D:\yandexdisk\disksync\YandexDisk\work\Другое\OtherUsers\1"
-xcopy "D:\gitupload\CheckHelp\coreV2\CheckHelp\CheckHelp\bin\Release\net8.0\publish\win-x64\CheckHelp.exe" "D:\gitupload\CheckHelp"
-chcp 866
-pause
